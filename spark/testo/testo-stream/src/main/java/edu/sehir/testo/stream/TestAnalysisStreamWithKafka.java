732679ef461e8b717846ed5a89afaa81a2b33e22
Emrullah Delibas <emrullah.delibas@kdv.com.tr>
2017-05-09 20:24:03 +0300

    null value bug fix

--- spark/testo/testo-stream/src/main/java/edu/sehir/testo/stream/TestAnalysisStreamWithKafka.java
+++ spark/testo/testo-stream/src/main/java/edu/sehir/testo/stream/TestAnalysisStreamWithKafka.java
@@ -86,7 +86,10 @@ public final class TestAnalysisStreamWithKafka {
             public Void call(JavaPairRDD<String, Vector> stringVectorJavaPairRDD) throws Exception {
                 stringVectorJavaPairRDD.foreach(new VoidFunction<Tuple2<String, Vector>>() {
                     public void call(Tuple2<String, Vector> stringVectorTuple2) throws Exception {
-                        JavaRDD<String> testerValue = jsc.parallelize(Arrays.asList(stringVectorTuple2._2().toString()));
+                        String value = stringVectorTuple2._2().toString();
+                        if (value == null)
+                            value = "";
+                        JavaRDD<String> testerValue = jsc.parallelize(Arrays.asList(value));
                         testerValue.saveAsTextFile(Path.getPath(Globals.HDFS_PROCESSED_FILE_DIR, stringVectorTuple2._1(), Globals.TEST_FILE_NAME_FORMAT.format(new Date())));
 //                        Writer.saveAsTextFile(
 //                                Globals.HDFS_PROCESSED_FILE_DIR,
